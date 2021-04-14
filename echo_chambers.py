eaab2529ef1dc3fb6c7b55cc600af5bc3686e7ab
Jace Laquerre <jace.laquerre@uvm.edu>
2021-04-14 16:56:41 -0400

    extracting edge from code

--- echo_chambers.py
+++ echo_chambers.py
@@ -1,7 +1,32 @@
 # CS395 Final
 
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+IN_FILENAME = "tweets.csv"
+OUT_FILENAME = "edges.csv"
+
+KEYWORDS = ['illegal', 'aliens', 'refugees', 'biden', 'trump', 'jobs', 'wall', 'greencard', 'visa', 'citizen', 'US',
+            '#illegal', '#aliens', '#refugees', '#biden', '#trump', '#jobs', '#wall', '#greencard', '#visa', '#citizen',
+            '#US', '#undocumented', 'undocumented']
 
 def main():
-    x = 0
+
+        graph = nx.Graph()
+        with open(IN_FILENAME, "r", encoding='utf-8') as csvInFile:
+            writeFile = open(OUT_FILENAME, "w", encoding='utf-8')
+            csvReader = csv.reader(csvInFile)
+            next(csvReader)
+            for row in csvReader:
+                print(row[0], row[3].lower())
+                splitStrs = row[3].lower().split(" ")
+                for word in splitStrs:
+                    if word in KEYWORDS:
+                        writeFile.write(row[0].strip('"'))
+                        writeFile.write(", ")
+                        writeFile.write(word)
+                        writeFile.write('\n')
+
 
 main()
