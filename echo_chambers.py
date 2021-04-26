0a8997b002ccb10a09bd2c41ea80d468af56cdc5
Jace Laquerre <jace.laquerre@uvm.edu>
2021-04-26 18:29:44 -0400

    final changes

--- echo_chambers.py
+++ echo_chambers.py
@@ -12,11 +12,10 @@ RESULTS_FILENAME = "results.txt"
 
 KEYWORDS = ['illegal', 'aliens', 'refugees', 'biden', 'trump', 'jobs', 'wall', 'greencard', 'visa', 'citizen', 'US',
             '#illegal', '#aliens', '#refugees', '#biden', '#trump', '#jobs', '#wall', '#greencard', '#visa', '#citizen',
-            '#US', '#undocumented', 'undocumented']
+            '#US', 'USA', '#USA', '#undocumented', 'undocumented', 'law', 'order', '#law', '#order']
 
-BETTER_KEYWORDS = ['immigration', '#immigration', 'residents', '#residents', 'immigrants', '#immigrants']
-
-
+BETTER_KEYWORDS = ['immigration', '#immigration', 'residents', '#residents', 'immigrants', '#immigrants',
+                   'migrants', '#migrants']
 
 def main():
     # Create a dictionary for the words
@@ -30,20 +29,16 @@ def main():
         # For every line in the CSV read in print the row
         for col in csvReader:
             if col[0] != '':
-                #print(col[0], col[3].lower())
                 splitStrs = col[3].lower().split(" ")
                 for word in splitStrs:
                     if word in KEYWORDS:
                         writeFile.write(col[0].strip('"'))
                         writeFile.write(",")
                         if word[0] == '#':
-                            #writeFile.write(str(word[1:]))
-                            # Add an index instead of a hash word
                             hashed_word = str(hash(word[1:]))
                             word_dict[str(word[1:])] = hashed_word
                             writeFile.write(hashed_word)
                         else:
-                            #writeFile.write(str(word))
                             hashed_word = str(hash(word))
                             word_dict[str(word)] = hashed_word
                             writeFile.write(hashed_word)
@@ -58,7 +53,6 @@ def main():
     G = nx.parse_edgelist(Data, 't', ',', Graphtype, int, (('Keyword', float),))
     for line in Data:
         x = line.rstrip().split(',')
-        #print(x[0], x[1])
 
     # Create graph
     nx.draw_networkx(G,
@@ -79,14 +73,7 @@ def main():
     # Find communities
     communities = list(greedy_modularity_communities(G))
 
-    # Get largest community
-    largest_community = (sorted(communities[0]))
-
     nx.edges(G)
-
-    # Loop through nodes to write them to file
-    # for i in range(len(largest_community)):
-    #     resultsFile.write(str(largest_community[i]) + "\n")
 
     resultsFile.write("# of communities: " + str(len(communities)) + "\n")
 
@@ -102,26 +89,20 @@ def main():
     avg_clus = nx.average_clustering(G)
     # Write Results to file
     resultsFile.write("average clustering: " + str(avg_clus) + "\n")
-
-    # hex_string = "0x616263"
-     #bytes_object = bytes.fromhex(hex_string)
-     #ascii_string = bytes_object.decode("ASCII")
-    # print(ascii_string)
-
+    # Close File
     resultsFile.close()
 
-    ## //////////////////////////////////////////////////////////////////////////////////
-    ## BETTER KEYWORDS
-    ## //////////////////////////////////////////////////////////////////////////////////
+    # //////////////////////////////////////////////////////////////////////////////////
+    # BETTER KEYWORDS
+    # //////////////////////////////////////////////////////////////////////////////////
     with open(IN_FILENAME, "r", encoding='utf-8') as csvInFile:
-        writeFile = open(OUT_FILENAME, "w", encoding='utf-8')
+        writeFile = open(BETTER_OUT_FILENAME, "w", encoding='utf-8')
         csvReader = csv.reader(csvInFile)
         next(csvReader)
         writeFile.write("ID,Keyword\n")
         # For every line in the CSV read in print the row
         for col in csvReader:
             if col[0] != '':
-                #print(col[0], col[3].lower())
                 splitStrs = col[3].lower().split(" ")
                 for word in splitStrs:
                     if word in BETTER_KEYWORDS:
@@ -133,7 +114,7 @@ def main():
                             writeFile.write(str(hash(word)))
                         writeFile.write('\n')
     writeFile.close()
-    Data = open(OUT_FILENAME, "r")
+    Data = open(BETTER_OUT_FILENAME, "r")
     next(Data, None)  # skip the first line in the input file
     Graphtype = nx.Graph()
 
@@ -141,7 +122,6 @@ def main():
     G = nx.parse_edgelist(Data, 't', ',', Graphtype, int, (('Keyword', float),))
     for line in Data:
         x = line.rstrip().split(',')
-        #print(x[0], x[1])
 
     # Create graph
     nx.draw_networkx(G,
@@ -163,14 +143,7 @@ def main():
     resultsFile.write("//////////////////////////////////////////////////\n")
     # Find communities
     communities = list(greedy_modularity_communities(G))
-
-    # Get largest community
-    largest_community = (sorted(communities[0]))
     nx.edges(G)
-
-    # Loop through nodes to write them to file
-    # for i in range(len(largest_community)):
-    #     resultsFile.write(str(largest_community[i]) + "\n")
 
     resultsFile.write("# of communities: " + str(len(communities)) + "\n")
 
@@ -186,6 +159,7 @@ def main():
     avg_clus = nx.average_clustering(G)
     # Write Results to file
     resultsFile.write("average clustering: " + str(avg_clus) + "\n")
-
+    # Close file
     resultsFile.close()
+
 main()
